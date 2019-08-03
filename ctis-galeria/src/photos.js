33a3d2c63cb1531eab6dd5ff7d034f6ec3c2c257
Alberto Labuto <albertolabuto@gmail.com>
2019-08-03 02:32:16 -0300

    Add files via upload

---
+++ ctis-galeria/src/photos.js
@@ -0,0 +1,60 @@
+export const photos = [
+  {
+    src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
+    thumburl: "https://via.placeholder.com/150/92c952",
+    width: 1,
+    height: 1,
+    title: "titulo",
+    alt: "teste123"
+  },
+  {
+    src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
+    thumburl: "https://via.placeholder.com/150/771796",
+    width: 1,
+    height: 1,
+    alt: "teste123"
+  },
+  {
+    src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+    width: 1,
+    height: 1,
+    alt: "teste123"
+  },
+  {
+    src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+    width: 1,
+    height: 1,
+    alt: "teste123"
+  },
+  {
+    src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
+    width: 1,
+    height: 1,
+    alt: "teste123"
+  },
+  {
+    src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+    width: 1,
+    height: 1,
+    alt: "teste123"
+  },
+  {
+    src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
+    width: 1,
+    height: 1,
+    alt: "teste123"
+  },
+  {
+    src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+    width: 1,
+    height: 1,
+    alt: "teste123"
+  },
+  {
+    src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+    width: 1,
+    height: 1,
+    alt: "teste123"
+  }
+];
+
